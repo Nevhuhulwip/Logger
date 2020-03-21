c5c2e80cb04feaedd749636c24466f8af967100c
Nevhuhulwip <pfanonevhuhulwi@gmail.com>
2020-03-21 07:31:09 +0200

    Logger Project

--- src/main/java/PasswordChecker.java
+++ src/main/java/PasswordChecker.java
@@ -18,32 +18,32 @@ public class PasswordChecker {
         if (yourPassword.isEmpty()) {
             logger.error("password does not exist ");
             flag = false;
-//            listOfErrors.add("password does not exist ");
+
         }
         if (yourPassword.length() < 8) {
             logger.error("Password length must have at least 8 character ");
             flag = false;
-//            listOfErrors.add("Password length must have at least 8 character ");
+
         }
         if (!specialCharPatten.matcher(yourPassword).find()) {
             logger.error("Password must have at least one special character ");
             flag = false;
-//            listOfErrors.add("Password must have at least one special character ");
+
         }
         if (!UpperCasePatten.matcher(yourPassword).find()) {
             logger.error("Password must have at least one uppercase character ");
             flag = false;
-//            listOfErrors.add("Password must have at least one uppercase character ");
+
         }
         if (!lowerCasePatten.matcher(yourPassword).find()) {
             logger.error("Password must have at least one lowercase character ");
             flag = false;
-//            listOfErrors.add("Password must have at least one lowercase character ");
+
         }
         if (!digitCasePatten.matcher(yourPassword).find()) {
             logger.error("Password must have at least one digit character ");
             flag = false;
-//            listOfErrors.add("Password must have at least one digit character ");
+
         }
         return flag;
     }
@@ -60,8 +60,7 @@ public class PasswordChecker {
         boolean flag = false;
         if (password.isEmpty() && password.length() < 8) {
             return flag;
-//        }else{
-//            System.out.println("Password meets condition 1 and 2");
+
        }
             return flag;
 
